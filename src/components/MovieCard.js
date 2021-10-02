f1e7d0667dc659e4db6095545a9157ccbb9095f1
git\Aditya Nayak <nayak.aditya998@gmail.com>
2021-10-01 23:15:25 -0700

    Added the functionality to unfavourite movie and changing tabs

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { addFavourite, removeFromFavourites } from '../actions'
 
 
 
 class MovieCard extends React.Component {
 
+    handeFavouriteClick = () => {
+        const { movie } = this.props;
+        this.props.dispatch(addFavourite(movie));
+    }
+
+    handeUnFavouriteClick = () => {
+        const { movie } = this.props;
+        this.props.dispatch(removeFromFavourites(movie));
+    }
     render (){
-        const {movie} = this.props;
+        const {movie, isFavourite} = this.props;
         return (
             <div className="movie-card">
                 <div className="left">
@@ -17,7 +27,12 @@ class MovieCard extends React.Component {
                     <div className="plot">{movie.Plot}</div>
                     <div className="footer">
                         <div className="rating">{movie.imdbRating}</div>
-                        <button className="favourite-btn">Favourite</button>
+                        {
+                            isFavourite
+                            ? <button className="unfavourite-btn" onClick={this.handeUnFavouriteClick}>UnFavourite</button>
+                            : <button className="favourite-btn" onClick={this.handeFavouriteClick}>Favourite</button>
+                        }
+                        
                     </div>
                 </div>
               
