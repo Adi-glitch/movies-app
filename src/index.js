9da59fa850edf5519a91b3a2e5607af3fa53ef58
git\Aditya Nayak <nayak.aditya998@gmail.com>
2021-10-15 12:03:06 +0530

    Add search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 
 import './index.css';
@@ -8,8 +9,37 @@ import App from './components/App';
 import rootReducer from './reducers';
 
 
+//function logger(obj, next, action)
+//logger(obj)(next)(action)
+// const logger = function ({dispatch, getState}){
+//   return function (next){
+//     return function (action){
+//       //middleware code
+//       console.log('ACTION_TYPE = ',action.type);
+//       next(action);
+//     }
+//   }
+// }
 
-const store = createStore(rootReducer);
+const logger = ({dispatch, getState}) => (next) => (action) => {
+  //logger code
+  if(typeof action.type === 'function'){
+    console.log('ACTION_TYPE = ',action.type);
+  }
+  
+    next(action);
+}
+
+// const thunk = ({dispatch, getState}) => (next) => (action) => {
+//   //logger code
+//   if (typeof action == 'function') {
+//     action(dispatch);
+//     return;
+//   }
+//     next(action);
+// }
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 console.log(store);
 // console.log('BEFORE STATE',store.getState());
 
@@ -28,6 +58,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES, ADD_MOVIE_TO_LIST } from '../actions';
+import { ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES, ADD_MOVIE_TO_LIST, ADD_SEARCH_RESULT } from '../actions';
 
 const initialMovieState = {
     list: [],
@@ -45,7 +45,13 @@ export function movies(state = initialMovieState, action){
                 return{
                     ...state,
                     showFavourites: action.val
-                }     
+                }   
+            
+            case ADD_MOVIE_TO_LIST:
+                return { 
+                    ...state,
+                    list: [action.movie, ...state.list]
+                };
 
             default:
                 return state;
@@ -53,17 +59,34 @@ export function movies(state = initialMovieState, action){
 }
 
 const initialSearchState = {
-    result: {}
+    result: {}, 
+    searchshowSearchResults: false
 }
 export function search(state = { result: initialSearchState }, action){
-    console.log('SEARCH_REDUCER');
-    return state;
+    
+    switch(action.type){
+        case ADD_SEARCH_RESULT:
+            return { 
+                ...state,
+                result: action.movie,
+                showSearchResults: true
+            }
+
+            case ADD_MOVIE_TO_LIST:
+                return { 
+                    ...state,
+                    showSearchResults: false
+                };    
+            
+            default:
+                return state;
+    }
 }
 
-const initialRootState = {
-    movies: initialMovieState,
-    search: initialSearchState
-}
+// const initialRootState = {
+//     movies: initialMovieState,
+//     search: initialSearchState
+// }
 
 // export default function rootReducer(state = initialRootState, action){
 //     return {
